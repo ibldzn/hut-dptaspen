c4cf689eb54c16a0d8260d9fd596192d950d41b3
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-01-23 09:10:09 +0700

    Update invitation rendering to use dynamic dress code value

--- internal/adapter/http/handler/invitation.go
+++ internal/adapter/http/handler/invitation.go
@@ -3,8 +3,20 @@ package handler
 import "net/http"
 
 func (h *Handler) RenderInvitationPage(w http.ResponseWriter, r *http.Request) {
+	guest := r.URL.Query().Get("name")
+	dressCode := "Bebas Rapih"
+
+	if emp, err := h.cfg.EmpService.GetEmployeeByName(r.Context(), guest); err == nil && emp != nil {
+		dressCode = "Lihat Surat Edaran"
+	}
+
+	data := map[string]any{
+		"DressCode": dressCode,
+	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if err := h.templates.ExecuteTemplate(w, "invitation.gohtml", nil); err != nil {
+
+	if err := h.templates.ExecuteTemplate(w, "invitation.gohtml", data); err != nil {
 		http.Error(w, "failed to render invitation page", http.StatusInternalServerError)
 		return
 	}
